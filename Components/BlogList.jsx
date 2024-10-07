97c12cc019b5f0ee3b97bfe3713d2eaa6445179b
Kinolog76 <vladpasha12@gmail.com>
2024-10-07 20:03:47 +0300

    Add fiter to BlogList

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -1,28 +1,61 @@
 import BlogItem from "./BlogItem";
 import { blog_data } from "@/assets/assets";
+import { useState } from "react";
 
 function BlogList() {
+  const [menu, setMenu] = useState("All");
+
+  const handleMenu = (category) => {
+    setMenu(category);
+    console.log(category);
+  };
+
   return (
     <div>
       <div className="flex gap-6 my-10 justify-center">
-        <button className="bg-black text-white px-4 py-2 rounded-sm">
+        <button
+          onClick={() => handleMenu("All")}
+          className={`${
+            menu === "All"
+              ? "bg-black text-white border"
+              : "border border-transparent hover:border-black"
+          } px-4 py-2 transition-all duration-300`}>
           All
         </button>
-        <button className="border border-transparent hover:border-black px-4 py-2 transition-all duration-300">
+        <button
+          onClick={() => handleMenu("Technology")}
+          className={`${
+            menu === "Technology"
+              ? "bg-black text-white border"
+              : "border border-transparent hover:border-black"
+          } px-4 py-2 transition-all duration-300`}>
           Technology
         </button>
-        <button className="border border-transparent hover:border-black px-4 py-2 transition-all duration-300">
+        <button
+          onClick={() => handleMenu("Startup")}
+          className={`${
+            menu === "Startup"
+              ? "bg-black text-white border"
+              : "border border-transparent hover:border-black"
+          } px-4 py-2 transition-all duration-300`}>
           Startup
         </button>
-        <button className="border border-transparent hover:border-black px-4 py-2 transition-all duration-300">
+        <button
+          onClick={() => handleMenu("Lifestyle")}
+          className={`${
+            menu === "Lifestyle"
+              ? "bg-black text-white border"
+              : "border border-transparent hover:border-black"
+          } px-4 py-2 transition-all duration-300`}>
           Lifestyle
         </button>
       </div>
 
       <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
-        {blog_data.map((item) => {
-          return <BlogItem key={item.id} {...item} />
-        })}
+        {blog_data
+          .filter((item) => menu === "All" ? item : item.category === menu).map((item) => {
+            return <BlogItem key={item.id} {...item} />;
+          })}
       </div>
     </div>
   );
