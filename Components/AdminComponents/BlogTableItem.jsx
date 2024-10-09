97d4c73b1edc1547d0868ec09f0dc828afcf4000
Kinolog76 <vladpasha12@gmail.com>
2024-10-09 14:50:42 +0300

    Create BlogList for admin

---
+++ Components/AdminComponents/BlogTableItem.jsx
@@ -0,0 +1,29 @@
+import { assets } from "@/assets/assets"
+import Image from "next/image"
+
+function BlogTableItem ({authorImg, author, title, date}) {
+  const formatDate = (date) => {
+    const newDate = new Date(date)
+    const options = { year: 'numeric', month: 'short', day: 'numeric' }
+    return newDate.toLocaleDateString('en-US', options)
+  }
+  return (
+    <tr className="bg-white border-b">
+      <th scope="row" className="items-center px-6 py-4 gap-3 sm:flex font-medium text-gray-900 whitespace-nowrap">
+        <Image src={authorImg?authorImg:assets.profile_icon} alt="" width={40} height={40}/>
+        <p>{author?author:"No author"}</p>
+      </th>
+      <td className="px-6 py-4">
+        {title?title:"No title"}
+      </td>
+      <td className="px-6 py-4 text-center">
+        {date?formatDate(date):"No date"}
+      </td>
+      <td className="px-6 py-4 text-center cursor-pointer font-extrabold text-red-500">
+        X
+      </td>
+    </tr>
+  )
+};
+
+export default BlogTableItem
--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,11 +1,45 @@
 "use client"
+import BlogTableItem from "@/Components/AdminComponents/BlogTableItem"
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function page () {
+function BlogList () {
+  const [blogsData, setBlogsData] = useState([])
+
+  const fetchBlogsData = async () => {
+    const response = await axios.get("/api/blog")
+    console.log(response.data);
+    setBlogsData(response.data.blogs)
+  }
+
+  useEffect(() => {
+    fetchBlogsData()
+  }, [])
+
   return (
-    <>
-      
-    </>
+    <div className="flex flex-col pt-5 px-5 sm:pt-12 sm:-l-16">
+        <h1>All blogs</h1>
+        <div className="relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide">
+          <table className="w-full text-sm text-gray-500">
+            <thead className="text-sm text text-gray-100 uppercase bg-gray-700">
+              <tr>
+                <th scope="col" className="hidden sm:block px-6 py-3">Author name</th>
+                <th scope="col" className="px-6 py-3">Title</th>
+                <th scope="col" className="px-6 py-3">Date</th>
+                <th scope="col" className="px-6 py-3">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {blogsData
+                .sort((a, b) => new Date(b.date) - new Date(a.date))
+                .map((blog) => {
+                  return <BlogTableItem key={blog._id} {...blog}/>
+                })}
+            </tbody>
+          </table>
+        </div>
+    </div>
   )
 };
 
-export default page
+export default BlogList
